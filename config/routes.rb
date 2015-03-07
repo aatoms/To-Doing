d60cf6309941f52b9ee0ffc3a169ba92c5dd98c1
Austin Adams <austin.atoms@gmail.com>
2015-03-07 16:23:15 -0700

    Users can see list of projects and add a new project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
   get '/terms' => 'terms#index'
   get '/about' => 'about#index'
   get '/faq' => 'common_questions#index'
@@ -7,7 +8,10 @@ Rails.application.routes.draw do
   get '/about' => 'about#index'
   get '/users' => 'users#index'
   get '/projects' => 'projects#index'
+
+
   resources :tasks
   resources :users
   resources :projects
+
 end
