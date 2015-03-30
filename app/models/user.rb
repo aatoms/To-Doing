6815ab0b9f56755df961ff99dbaf448e7858de2b
Austin Adams <austin.atoms@gmail.com>
2015-03-29 21:57:24 -0600

    Data integrity; User can delete projects from show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
-  has_many :memberships
+  has_many :memberships, :dependent => :destroy
   has_many :projects, through: :memberships
-  has_many :comments
+  has_many :comments, :dependent => :nullify
   has_secure_password
 
   def full_name
