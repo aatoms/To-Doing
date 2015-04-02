a8316cb38fd713b5fb21d73f0d263e4d34dd3cfe
Austin Adams <austin.atoms@gmail.com>
2015-04-02 08:50:23 -0600

    Task comments dependent

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
   ROLES = ['Member', 'Owner']
-
+  
   validates :user_id, presence: true
   validates :user_id, uniqueness: { scope: :project_id, message: "has already been added to this project"}
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   validates :description, :due_date, presence: true
   belongs_to :project
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 end
