c1725c9ae1434ceac37e001cb1ef315303e6ad54
Austin Adams <austin.atoms@gmail.com>
2016-04-30 00:04:01 -0700

    To-Doing

--- app/controllers/common_questions_controller.rb
+++ app/controllers/common_questions_controller.rb
@@ -1,8 +1,8 @@
 class CommonQuestionsController < ApplicationController
   def index
-    c_question_1 = CommonQuestion.new("What is gCamp?", "gCamp is an awesome tool that is going to change your life. gCamp is your one stop shop to organize all your tasks. You'll also be able to track comments that you and others make. gCamp may eventually replace all need for paper and pens in the entire world. Well, maybe not, but it's going to be pretty cool.", "what-is-gcamp")
-    c_question_2 = CommonQuestion.new("How do I join gCamp?", "As soon as it's ready for the public, you'll see a signup link in the upper right. Once that's there, just click it and fill in the form!", "how-do-i-join-gcamp")
-    c_question_3 = CommonQuestion.new("When will gCamp be finished?", "gCamp is a work in progress. That being said, it should be fully functional in the next few weeks. Functional. Check in daily for new features and awesome functionality. It's going to blow your mind. Organization is just a click away. Amazing!", "when-will-gcamp-be-finished")
+    c_question_1 = CommonQuestion.new("What is To-Doing?", "To-Doing is an awesome tool that is going to change your life. To-Doing is your one stop shop to organize all your tasks. You'll also be able to track comments that you and others make. To-Doing may eventually replace all need for paper and pens in the entire world. Well, maybe not, but it's going to be pretty cool.", "what-is-To-Doing")
+    c_question_2 = CommonQuestion.new("How do I join To-Doing?", "As soon as it's ready for the public, you'll see a signup link in the upper right. Once that's there, just click it and fill in the form!", "how-do-i-join-To-Doing")
+    c_question_3 = CommonQuestion.new("When will To-Doing be finished?", "To-Doing is a work in progress. That being said, it should be fully functional in the next few weeks. Functional. Check in daily for new features and awesome functionality. It's going to blow your mind. Organization is just a click away. Amazing!", "when-will-To-Doing-be-finished")
 
         @faqs = [c_question_1, c_question_2, c_question_3]
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < PublicController
 
    def index
-     @quotes_array=[{"quote"=>"gCamp has changed my life! It\'s the best tool I\'ve ever used.", "contributor"=>"Cayla Hayes"},
-       {"quote"=>'"Before gCamp I was a disorderly slob. Now I\'m more organized than I\'ve ever been"', "contributor"=>"Leta Jaskolski"},
+     @quotes_array=[{"quote"=>"To-Doing has changed my life! It\'s the best tool I\'ve ever used.", "contributor"=>"Cayla Hayes"},
+       {"quote"=>'"Before To-Doing I was a disorderly slob. Now I\'m more organized than I\'ve ever been"', "contributor"=>"Leta Jaskolski"},
        {"quote"=>'"Don\'t hesitate - sign up right now! You\'ll never be the same."', "contributor"=>"Lavern Upton"}]
    end
 end
